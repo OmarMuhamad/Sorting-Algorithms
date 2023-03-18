2afd5c0d40389b23b7e324f88955ff5c0f54259a
Omar Mohammad <101476960+OmarMuhamad@users.noreply.github.com>
2023-03-18 20:45:42 +0200

    first update

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+namespace sortlib {
+	template<typename Type>
+	void countSort() {
+
+        }
+	}
+	template<typename Type>
+	void mergeSort() {
+
+	}
+    template <typename Type>
+	void insertionSort() {
+
+	}
+    template<typename Type>
+	void quickSort() {
+
+	}
+    template<typename Type>
+	void bubbleSort() {
+
+	}
+	template<typename Type>
+    void selectionSort(){
+
+    }
+    template<typename Type>
+    void shellSort(){
+
+    }
+}
+int main() {
+
+}
